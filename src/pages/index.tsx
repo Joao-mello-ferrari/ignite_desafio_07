cdd985a4002084d826d69f9874f6b653bb9e1741
João Mello <joaomello@joaos-mac.local>
2022-11-23 10:38:26 -0300

    Fixing bugs

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import { Button, Box } from '@chakra-ui/react';
 import { useMemo } from 'react';
 import { useInfiniteQuery } from 'react-query';
@@ -18,14 +19,28 @@ export default function Home(): JSX.Element {
     hasNextPage,
   } = useInfiniteQuery(
     'images',
+    async () => {
+      const r = await api.get('http://localhost:3000/api/images');
+      return r.data.data;
+    }
     // TODO AXIOS REQUEST WITH PARAM
-    ,
+    // ,
     // TODO GET AND RETURN NEXT PAGE PARAM
   );
 
-  const formattedData = useMemo(() => {
-    // TODO FORMAT AND FLAT DATA ARRAY
-  }, [data]);
+  console.log(data, hasNextPage);
+
+  // async function a(): Promise<void> {
+  //   const r = await api.get('http://localhost:3000/api/images');
+  //   console.log(r);
+  // }
+
+  // a();
+
+  // const formattedData = useMemo(() => {
+  //   return data;
+  //   // TODO FORMAT AND FLAT DATA ARRAY
+  // }, [data]);
 
   // TODO RENDER LOADING SCREEN
 
@@ -36,7 +51,14 @@ export default function Home(): JSX.Element {
       <Header />
 
       <Box maxW={1120} px={20} mx="auto" my={20}>
-        <CardList cards={formattedData} />
+        {/* <CardList cards={formattedData} /> */}
+        <Image
+          src="https://i.ibb.co/zW3fdZn/Eu-com-filtro-leve.jpg"
+          alt="a1"
+          width="100"
+          height="100"
+          // fill="fixed"
+        />
         {/* TODO RENDER LOAD MORE BUTTON IF DATA HAS NEXT PAGE */}
       </Box>
     </>
